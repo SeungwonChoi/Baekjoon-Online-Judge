5ae1334421e098c19940d087c3ffb460b453d32f
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-13 23:55:32 +0900

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -1,5 +1,5 @@
 # https://www.acmicpc.net/problem/11399
 
-user_num = int(input())
+input()
 times = sorted(list(map(int, input().split())))
-print(sum(times[i] * (len(times) - i) for i in range(0, len(times))))
+print(sum(times[i] * (len(times) - i) for i in range(len(times))))
