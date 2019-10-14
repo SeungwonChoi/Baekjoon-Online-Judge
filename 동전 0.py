1e76fb564f3745bce5dfcf041ee381de8cad52af
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-14 23:49:27 +0900

    Update 동전 0.py

--- 동전 0.py
+++ 동전 0.py
@@ -3,12 +3,14 @@
 type_num, target_value = map(int, input().split())
 values = [0] * type_num
 max_idx = type_num - 1
+
 for i in range(type_num):
     values[i] = int(input())
     if values[i] > target_value:
         max_idx = i - 1
         break
 cnt = 0
+
 for i in range(max_idx, -1, -1):
     cnt += target_value // values[i]
     target_value = target_value % values[i]
