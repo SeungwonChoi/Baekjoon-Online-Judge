14e6eb003b0071e02d61a167109437debddb300e
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-21 00:02:12 +0900

    Update 사칙연산.py

--- 입출력과 사칙연산/사칙연산.py
+++ 입출력과 사칙연산/사칙연산.py
@@ -1,1 +1,8 @@
 # https://www.acmicpc.net/problem/10869
+
+input = input().split()
+print(eval('+'.join(input)))
+print(eval('-'.join(input)))
+print(eval('*'.join(input)))
+print(eval('//'.join(input)))
+print(eval('%'.join(input)))
