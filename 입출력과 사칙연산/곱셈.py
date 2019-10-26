cb009e43c806399464c8342b2b2dc5eda1f86ba1
SeungwonChoi <35623289+SeungwonChoi@users.noreply.github.com>
2019-10-26 23:38:42 +0900

    Update 곱셈.py

--- 입출력과 사칙연산/곱셈.py
+++ 입출력과 사칙연산/곱셈.py
@@ -1,2 +1,5 @@
 # https://www.acmicpc.net/problem/2588
 
+num_1 = int(input())
+num_2_str = input()
+print(num_1 * int(num_2_str[2]), num_1 * int(num_2_str[1]), num_1 * int(num_2_str[0]), num_1 * int(num_2_str))
